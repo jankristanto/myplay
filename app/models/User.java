9a1103bf867977070c7f260518366dfed22b9ffa
Jan Kristanto <jan@ontelstudio.com>
2014-05-12 23:15:24 +0700

    myplay initial version

---
+++ app/models/User.java
@@ -0,0 +1,16 @@
+package models;
+import play.modules.morphia.Model;
+import com.google.code.morphia.annotations.Entity;
+@Entity
+public class User extends Model {
+ 
+    public String email;
+    public String password;
+    public String fullname;
+    
+    public User(String email, String password, String fullname) {
+        this.email = email;
+        this.password = password;
+        this.fullname = fullname;
+    }
+}
---
+++ test/BasicTest.java
@@ -0,0 +1,24 @@
+import org.junit.*;
+import java.util.*;
+import play.test.*;
+import models.*;
+
+public class BasicTest extends UnitTest {
+
+    @Test
+    public void aVeryImportantThingToTest() {
+        assertEquals(2, 1 + 1);
+    }
+    @Test public void createAndRetrieveUser() {
+    // Create a new user and save it
+    new User("bob@gmail.com", "secret", "Bob").save();
+    
+    // Retrieve the user with bob username
+    User bob = User.find("byEmail", "bob@gmail.com").first();
+    
+    // Test 
+    assertNotNull(bob);
+    assertEquals("Bob", bob.fullname);
+}
+
+}
